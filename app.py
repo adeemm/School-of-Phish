2f7f6fa2a9fce9b0c15a3f90c11b5bc23b82c435
Adeem <adeemmawani@gmail.com>
2021-03-25 17:04:04 -0400

    WSGI Compatibility Patch

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask_caching import Cache
 import settings
 
 
-flask_app = Flask(__name__)
+application = Flask(__name__)
 
 cache = Cache(config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': 86400})
-cache.init_app(flask_app)
+cache.init_app(application)
 
 settings.load_settings("settings.json")
 
 if __name__ == '__main__':
-    flask_app.run(use_debugger=True)
+    application.run(use_debugger=True)
 
 
 import controllers.views
