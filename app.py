ccee3c7d05d7540fb6b75b25df81cc8bbd8a534a
Adeem <adeemmawani@gmail.com>
2021-03-25 17:08:13 -0400

    Revert "WSGI Compatibility Patch"
    This reverts commit 2f7f6fa2a9fce9b0c15a3f90c11b5bc23b82c435.

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask_caching import Cache
 import settings
 
 
-application = Flask(__name__)
+flask_app = Flask(__name__)
 
 cache = Cache(config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': 86400})
-cache.init_app(application)
+cache.init_app(flask_app)
 
 settings.load_settings("settings.json")
 
 if __name__ == '__main__':
-    application.run(use_debugger=True)
+    flask_app.run(use_debugger=True)
 
 
 import controllers.views
